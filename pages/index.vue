6cebc5c1d09838a3152a83f6bd7635f92b382e37
fl_raimberdiyev <fl.raimberdiyev@gmail.com>
2024-11-28 19:31:26 +0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Posts></Posts>
+</template>
+
+<script setup lang="ts">
+  const { find } = useStrapi();
+  const { data } = await useAsyncData("posts", function () {
+    return find("posts", {
+      populate: "*",
+    });
+  });
+</script>
+
+<style scoped></style>
